29b4f499537de65a0c69add47a2f462cfb1c4ad4
Yaniv Amiri <Yaniv@eds-macbook-air.home>
2018-05-16 18:49:17 -0400

    Made my initial view of what I want. Obviously we will make it look better but this is a good start for now. Take a look

---
+++ MyGymBagAlpha/QuestionnaireVC.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  MyGymBagAlpha
+//
+//  Created by Yaniv Amiri on 5/16/18.
+//  Copyright © 2018 TGYA. All rights reserved.
+//
+
+import UIKit
+
+class QuestionnaireVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- MyGymBagAlpha/ViewController.swift
+++ MyGymBagAlpha/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? QuestionnaireVC {} else {return}
+    }
 
 }
 
