312144da31b745d2173fc39c91c4ed6caae34819
Yaniv Amiri <Yaniv@eds-macbook-air.home>
2018-05-26 11:19:44 -0400

    Added fancier looking cells that actually appear on the screen and added unwinding segue between questionnaire and suggestion views.

---
+++ MyGymBagAlpha/CustomWorkoutCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomWorkoutCell.swift
+//  MyGymBagAlpha
+//
+//  Created by Yaniv Amiri on 5/23/18.
+//  Copyright © 2018 TGYA. All rights reserved.
+//
+
+import UIKit
+
+class CustomWorkoutCell : UITableViewCell {
+    @IBOutlet weak var customView: UIView!
+    @IBOutlet weak var workoutTitle: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
--- MyGymBagAlpha/SuggestionVC.swift
+++ MyGymBagAlpha/SuggestionVC.swift
@@ -12,9 +12,33 @@ class SuggestionVC: UIViewController {
     var sex: String?
     var age: String?
     var numVisits: String?
+    var workouts = ["W0", "W1","W2","W3","W4","W5","W6","W7","W8","W9"]
+    var workoutDescription = ["Test Description", "Test Description", "Test Description", "Test Description", "Test Description", "Test Description", "Test Description","Test Description" ,"Test Description", "Test Description"]
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 }
 
+extension SuggestionVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return workouts.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : CustomWorkoutCell = tableView.dequeueReusableCell(withIdentifier: "workoutCell", for: indexPath) as! CustomWorkoutCell
+        
+        cell.customView.layer.cornerRadius = cell.customView.frame.height / 2
+        cell.workoutTitle.text = workouts[indexPath.row]
+        cell.descriptionLabel.text = workoutDescription[indexPath.row]
+        
+        return cell
+    }
+}
